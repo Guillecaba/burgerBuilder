328461e19e0edf42032f735beae10c89d818d610
Guillermo <guillermocaba95@gmail.com>
2019-10-29 21:44:25 -0300

    feat(NavigationItems, SideDrawer):Add NavigationItems & SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,16 @@
-import React  from 'react';
+import React from "react";
 
-import classes from  './Layout.css';
-import  Auxi from '../../hoc/Auxi';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
+import classes from "./Layout.css";
+import Auxi from "../../hoc/Auxi";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = ( props ) => (
-    <Auxi>
-    <Toolbar></Toolbar>
-    <main className={classes.Content}>
-        {props.children}
-    </main>
-    </Auxi>
+const layout = props => (
+  <Auxi>
+    <Toolbar />
+    <SideDrawer />
+    <main className={classes.Content}>{props.children}</main>
+  </Auxi>
 );
 
 export default layout;
