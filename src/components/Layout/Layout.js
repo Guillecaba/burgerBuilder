96f1185273f15cba87c599874d85ba21c56aad72
Guillermo <guillermocaba95@gmail.com>
2019-11-06 17:41:08 -0300

    (SideDrawer, Toolbar): Adjust responsive and add a Sidedrawer Toggle button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./Layout.css";
 import Auxi from "../../hoc/Auxi";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = props => (
-  <Auxi>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Auxi>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer:true
+  }
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState ({ showSideDrawer:false })
+  }
+
+  sideToggleClosedHandler = () => {
+    this.setState((prevState ) => {
+      return { showSideDrawer : !prevState.showSideDrawer };
+    })
+  }
+
+  render() {
+    return (
+      <Auxi>
+        <Toolbar drawerToggleClick = {this.sideToggleClosedHandler} />
+        <SideDrawer  open = { this.state.showSideDrawer } closed = { this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Auxi>
+    );
+  }
+}
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Toolbar.css';
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToogle from "../../Navigation/SideDrawer/DrawerToogle/DrawerToogle"
 
-const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <div>
-            MENU
-        </div>
-        <Logo />
-        <nav>
-            <NavigationItems />
-        </nav>
-    </header>
+const toolbar = props => (
+  <header className={classes.Toolbar}>
+    <DrawerToogle clicked = { props.drawerToggleClick } />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className = {classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
 );
 
 export default toolbar;
