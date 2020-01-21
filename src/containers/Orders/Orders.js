9ad381bab24e821474d1eb7253a383290c8c1026
Guillermo <guillermocaba95@gmail.com>
2020-01-21 07:50:02 -0300

    feat:Finish routing

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading:true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+        .then(res => {
+            console.log(res.data);
+            const fetchedOrders = [];
+            for ( let key in res.data ) {
+                console.log(res.data[key])
+                fetchedOrders.push({
+                    ...res.data[key],
+                    id:key
+                });
+            
+            }
+            console.log(fetchedOrders)
+            this.setState({loading:false, orders:fetchedOrders})
+        }).catch(error => {
+            this.setState({loading:false})
+        });
+    }
+
     render() {
         return(
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order key={order.id} ingredients={order.ingredients} price={+order.price} />
+                ))}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders,axios);
